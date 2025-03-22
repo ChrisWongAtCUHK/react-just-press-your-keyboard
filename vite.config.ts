0ade1f12bfcd8c7d5a01b29c69f2b47be504e04f
Chris Wong <chris.wong.ksl@gmail.com>
2025-03-22 19:26:50 +0800

    Install less

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        math: 'always',
+        relativeUrls: true,
+        javascriptEnabled: true,
+      },
+    },
+  },
 })
